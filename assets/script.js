3765551204615d8ebbbd0e57a98b11843c8b0df3
Khanh Bui <pkkhanhbui@gmail.com>
2022-10-03 00:00:42 -0400

    finished implementing LocalStorage

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,35 @@
 var today = moment();
 var divContainerEl = $('.container');
 var currentTime = moment().format('H');
+var storedNotes = [];
+
 var dayPlan = [
     {
-        time: 09,
-        notes: ''
+        time: 09        
     },
     {
-        time: 10,
-        notes: ''
+        time: 10        
     },
     {
-        time: 11,
-        notes: ''
+        time: 11        
     },
     {
-        time: 12,
-        notes: ''
+        time: 12        
     },
     {
-        time: 13,
-        notes: ''
+        time: 13        
     },
     {
-        time: 14,
-        notes: ''
+        time: 14        
     },
     {
-        time: 15,
-        notes: ''
+        time: 15        
     },
     {
-        time: 16,
-        notes: ''
+        time: 16        
     },
     {
-        time: 17,
-        notes: ''
+        time: 17        
     }
 ];
 
@@ -44,15 +37,17 @@ $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
 dayPlan.forEach((item, index) => {
     var timeblockrow = $('<div>').addClass('row');
-    var saveBtnEl = $('<button>').addClass('saveBtn col-md-1');
+    var saveBtnEl = $('<button>').addClass('saveBtn col-md-1').attr('id', 'btn-'+[index]);
     var hourEl = dayPlan[index].time;
     var hourDiv = $('<div>').addClass('hour time-block col-md-2');
     var timeEl = moment(hourEl, 'H').format('h A');
-    var noteTextAreaEl = $('<textarea>').addClass('description notes col-md-9').attr('id', [index]);
+    var noteTextAreaEl = $('<textarea>').addClass('description notes col-md-9').attr('id', 'ta-'+[index]);
+    storedNotes.push('');
     hourDiv.text(timeEl);
     saveBtnEl.html('<i class="fas fa-save"></i>');
     divContainerEl.append(timeblockrow);
     timeblockrow.append(hourDiv, noteTextAreaEl, saveBtnEl);
+
     if (hourEl < currentTime) {
         noteTextAreaEl.addClass('past')
     } else if (hourEl === currentTime) {
@@ -63,18 +58,22 @@ dayPlan.forEach((item, index) => {
 });
 
 
-
 $('.saveBtn').click(saveNotes);
 function init() {
-
+    var localNotes = JSON.parse(localStorage.getItem('notes'));
+    if (localNotes !== null) {
+        storedNotes = localNotes;
+        storedNotes.forEach((item, index) => {           
+            $('#ta-'+[index]).append(storedNotes[index]);
+        });     
+    };
 };
 
 function saveNotes() {
-    
-        var noteVal = $('#0').val();
-     //need to target by id.
-    //     localStorage.setItem('notes', JSON.stringify(noteVal))
-    console.log(noteVal)
+    var noteIndex = this.id[this.id.length -1];
+    var noteVal = $('#ta-'+ noteIndex).val();
+    storedNotes[noteIndex] = noteVal;
+    localStorage.setItem('notes', JSON.stringify(storedNotes));
 };
 
 init();
